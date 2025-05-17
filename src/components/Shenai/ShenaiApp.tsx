74358b4b99e81ccf74fe9f2d24c45fa6e5875786
Ashish Kushwaha <ashish@wishfy.club>
2025-05-17 17:01:26 +0530

    deinitialize shenai

--- src/components/Shenai/ShenaiApp.tsx
+++ src/components/Shenai/ShenaiApp.tsx
@@ -1,12 +1,24 @@
 // import { ShenaiSDKProvider } from "./ShenaiContext";
+import { useEffect, useState } from "react";
 import { ShenaiSDKView } from "./ShenaiSDKView";
 
 function ShenaiApp({ setStep, setIsShenaiInitialized }) {
+  const [shenAiSDK, setShenAiSDK] = useState(null);
+
+  useEffect(() => {
+    return () => {
+      if (shenAiSDK) {
+        shenAiSDK.deinitialize();
+      }
+    };
+  }, [shenAiSDK]);
+
   return (
     // <ShenaiSDKProvider>
     // </ShenaiSDKProvider>
     <ShenaiSDKView
       setStep={setStep}
+      setShenAiSDK={setShenAiSDK}
       setIsShenaiInitialized={setIsShenaiInitialized}
     />
   );
