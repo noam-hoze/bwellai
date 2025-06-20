eee3543eda561cd907eb38a7f88c057a80a3cf61
Yolanda Baron <yolanda@bwellai.com>
2025-06-19 23:12:03 -0400

    smallest transparent button

--- src/pages/MyGoals.tsx
+++ src/pages/MyGoals.tsx
@@ -62,7 +62,7 @@ const MyGoals = () => {
             className="bg-transparent text-transparent border-transparent hover:bg-transparent hover:text-transparent focus:outline-none"
           >
             <Plus className="mr-2 h-4 w-4 text-transparent" />
-            Create
+            
           </Button>
 
         </div>
