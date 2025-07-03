0ed02c59eabd94214d5b8f8f3453a26ea17efc99
noamhoze <noam.hoze@vayyar.com>
2025-07-03 18:25:46 -0400

    Remove mkcert

--- vite.config.ts
+++ vite.config.ts
@@ -22,7 +22,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 import { componentTagger } from 'lovable-tagger';
-import mkcert from 'vite-plugin-mkcert';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
@@ -46,7 +45,6 @@ export default defineConfig(({ mode }) => ({
   },
   plugins: [
     react(),
-    mkcert(),
     mode === 'development' && componentTagger(),
     {
       name: 'cross-origin-isolation-plugin',
